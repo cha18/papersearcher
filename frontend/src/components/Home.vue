f8cc692578119ec61cb0cb2deaf4b8e5315b3ed1
cha18 <realixemusic@gmail.com>
2024-01-21 01:55:22 +0530

    Initial commit

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -177,7 +177,7 @@ export default {
 
             this.articles=[];
             console.log(searchParams.toString())
-            fetch(`http://localhost:5000/search?${searchParams.toString()}`, {
+            fetch(`http://pastpaperapi.onrender.com/search?${searchParams.toString()}`, {
                 method: "GET",
                 headers: {
                 "Content-Type": "application/json",
