a647c981c78d90d30713cac11506b6dcec18eab6
cha18 <realixemusic@gmail.com>
2024-08-12 21:03:54 +0530

    adds parallel keep alive

--- backend/app.py
+++ backend/app.py
@@ -3,20 +3,18 @@ from flask_cors import CORS
 from pastpaperapi import create_app
 import time
 from keep_alive import keep_alive
+import threading
 
 app = create_app()
 
-while True:
-    keep_alive()
-    time.sleep(600)
+def background_task():
+    while True:
+        keep_alive()
+        time.sleep(600)
 
 if __name__ == '__main__':
+    thread = threading.Thread(target=background_task)
+    thread.daemon = True
+    thread.start()
+
     app.run(debug=True)
-
-
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
-
